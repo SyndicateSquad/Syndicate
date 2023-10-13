8201451b4d666b0b5526da0bac17690c9278ec8b
Deji1127 <aahmedibrahim2002@gmail.com>
2023-10-13 03:53:30 -0500

    sign in button

--- App.js
+++ App.js
@@ -14,6 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    backgroundColor: '#F9FBFC',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F9FBFC'
   },
 });
---
+++ src/components/CustomButton/CustomButton.js
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+import React from 'react'
+
+const CustomButton = ({ onPress, text }) => {
+    return (
+        <Pressable onPress={onPress} style={styles.container}>
+            <Text style={styles.text}> {text} </Text>
+        </Pressable>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#3B71F3',
+
+        width: '100%',
+        padding: 15,
+        marginVertical: 5,
+        alignItems: 'center',
+        borderRadius: 5,
+    },
+    text: {
+        fontWeight: 'bold',
+        color: 'white',
+    },
+});
+export default CustomButton
