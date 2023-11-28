029933bec0177729fc9de8c77db56108a553af14
Deji1127 <aahmedibrahim2002@gmail.com>
2023-11-28 15:13:27 -0600

    Update SocialSignUp.js

--- src/screens/SignUpScreen/SocialSignUp/SocialSignUp.js
+++ src/screens/SignUpScreen/SocialSignUp/SocialSignUp.js
@@ -14,38 +14,34 @@ const SocialSignUp = () => {
 
   return (
     <>
-      <View style={{ flexDirection: 'row', backgroundColor: '#FAE9AE', width: '100%' }}>
-        <CustomButton
-          text="Continue with Google"
-          onPress={onSignInGoogle}
-          bgColor="#FAE9AE"
-          fgColor="#DD4D44"
-        />
-        <MaterialCommunityIcons.Button
+      {/* <CustomButton
+        text="Continue with Google"
+        onPress={onSignInGoogle}
+        bgColor="#FAE9AE"
+        fgColor="#DD4D44"
+      /> */}
+      {/* <MaterialCommunityIcons.Button
           name='google'
           size={50}
           color='#DB4437'
           backgroundColor='transparent'
           underlayColor='transparent'
           activeOpacity={0.3}
-        />
-      </View>
-      <View>
-        <CustomButton
-          text="Continue with Apple"
-          onPress={onSignInApple}
-          bgColor="#e3e3e3"
-          fgColor="#363636"
-        />
-        <MaterialCommunityIcons.Button
+        /> */}
+      {/* <CustomButton
+        text="Continue with Apple"
+        onPress={onSignInApple}
+        bgColor="#e3e3e3"
+        fgColor="#363636"
+      /> */}
+      {/* <MaterialCommunityIcons.Button
           name='apple'
           size={50}
           backgroundColor='transparent'
           color='black'
           underlayColor='black'
           activeOpacity={0.3}
-        />
-      </View>
+        /> */}
     </>
   );
 };
