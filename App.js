2608bf725a6869ecb6c1d019014c7a1590a9f13a
Deji1127 <aahmedibrahim2002@gmail.com>
2023-12-02 19:02:58 -0600

    chat

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import Navigation from './src/navigation';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { StreamChat } from 'stream-chat';
+// import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { OverlayProvider } from 'stream-chat-react-native';
 
+const client = StreamChat.getInstance("5bbwwprpy2jr");
 
 const App = () => {
+  useEffect(() => {
+    const connectUser = async () => {
+      await client.connectUser(
+        {
+          id: 'ai212559',
+          name: 'Ahmed Ibrahim',
+        },
+        client.devToken('ai212559')
+      );
+      const channel = client.channel(
+        'messaging',
+        'syndicateTestChannel',
+        { name: "TestChannel", }
+      );
+      await channel.watch();
+    };
+
+    const disconnectClient = async () => {
+      await client.disconnectUser();
+    };
+
+    connectUser()
+    return () => {
+      disconnectClient();
+    };
+
+  }, []);
+
   return (
     <SafeAreaView style={styles.root}>
-      <Navigation />
+      <OverlayProvider>
+        <Navigation />
+      </OverlayProvider>
     </SafeAreaView>
   );
 };
@@ -15,7 +49,7 @@ const App = () => {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    backgroundColor: '#F9FBFC'
+    backgroundColor: '#F9FBFC',
   },
 });
 
